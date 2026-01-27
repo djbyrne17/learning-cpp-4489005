935dd8636bca85d0a7aa0d5d7300aa9d44965d38
DJ Byrne <mcgithubcom@unsupervised.space>
2026-01-27 00:44:25 +0000

    Implementation

--- src/Ch01/01_02b/CodeDemo.cpp
+++ src/Ch01/01_02b/CodeDemo.cpp
@@ -1,3 +1,9 @@
 // Learning C++ 
 // Exercise 01_02
 // Hello World, by Eduardo Corpeño 
+#include <iostream>
+int main (){
+   std::cout << "Hello, World!\n";
+   std::cout << std::endl << std::endl;
+   return EXIT_SUCCESS;
+}
